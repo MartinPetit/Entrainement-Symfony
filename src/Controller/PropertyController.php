fac47fecbbf529a617a2e5f3a3d4f05ce0c46732
MartinPetit <martinpetit1998@gmail.com>
2019-10-05 19:37:37 +0200

    entrainement bdd

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,9 +3,31 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class PropertyController extends AbstractController {
+
+	/**
+	*@var PropertyRepository
+	*/
+	private $repository;
+
+	/**
+	*@var ObjectManager
+	*/
+	private $em;
+
+
+	
+	public function __construct(PropertyRepository $repository, ObjectManager $em) 
+	{
+		$this->repository = $repository;
+		$this->em = $em;
+	}
+
+
 
 	/**
 	* @Route("/Biens", name = "property.index")
@@ -14,6 +36,8 @@ class PropertyController extends AbstractController {
 	
 	public function index() : Response
 	{
+
+		$property = $this->repository->findAllVisible(); 
 		return $this->render('property/index.html.twig', [
 			'current_menu' => 'properties'
 		]);
